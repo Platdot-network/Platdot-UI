5f3ac5083fe00c5489fab84263ca98bd0121344d
吴wbh1328551759 <1328551759@qq.com>
2021-06-08 16:45:12 +0800

    update ali-oss-webpack version

--- packages/apps/webpack.config.js
+++ packages/apps/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = merge(
       }),
       new AliosscdnWebpackPlugin({
         https: true,
-        directoryInOss: 'platdot/v1.1.8',
+        directoryInOss: 'platdot/v1.2.0',
         filesPath: `${__dirname}/build`,
         region: CLIENT.region,
         accessKeyId: CLIENT.accessKeyId,
